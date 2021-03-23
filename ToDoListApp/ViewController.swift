d50d422a6b2152dd496f26c45e448c1129b37940
szymoneg <szymon.zsp5@gmail.com>
2021-03-23 23:24:19 +0100

    init

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Szymon Biliński on 23/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
